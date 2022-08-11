7d4ac86721b34fe19caf51c687e5bbbb31e3a798
Peter Dragos <peter@mlabs.city>
2022-08-11 15:10:21 -0400

    update makefile, flake.nix, and do linting

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     nixpkgs.follows = "plutarch/nixpkgs";
-    nixpkgs-latest.url = "github:NixOS/nixpkgs?rev=a0a69be4b5ee63f1b5e75887a406e9194012b492";
+    nixpkgs-latest.url = "github:NixOS/nixpkgs";
     # temporary fix for nix versions that have the transitive follows bug
     # see https://github.com/NixOS/nix/issues/6013
     nixpkgs-2111 = { url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin"; };
+    nixpkgs-2205.url = "github:NixOS/nixpkgs/nixos-22.05";
 
     haskell-nix-extra-hackage.follows = "plutarch/haskell-nix-extra-hackage";
     haskell-nix.follows = "plutarch/haskell-nix";
@@ -15,7 +16,7 @@
 
     # Plutarch and its friends
     plutarch = {
-      url = "github:Plutonomicon/plutarch-plutus?ref=staging";
+      url = "github:Plutonomicon/plutarch-plutus?ref=master";
 
       inputs.emanote.follows =
         "plutarch/haskell-nix/nixpkgs-unstable";
@@ -30,7 +31,7 @@
     plutarch-context-builder.url =
       "github:Liqwid-Labs/plutarch-context-builder?ref=staging";
 
-    liqwid-nix.url = "github:Liqwid-Labs/liqwid-nix";
+    liqwid-nix.url = "github:Liqwid-Labs/liqwid-nix/emiflake/consistent-nixpkgs";
   };
 
   outputs = inputs@{ liqwid-nix, ... }:
@@ -47,15 +48,14 @@
           "${inputs.plutarch-numeric}"
           "${inputs.plutarch-context-builder}"
         ])
-        (liqwid-nix.addChecks {
-          testSuite = "liqwid-plutarch-extra:test:liqwid-plutarch-extra-test";
-          liqwid-plutarch-extra = "liqwid-plutarch-extra:lib:liqwid-plutarch-extra";
-        })
+        liqwid-nix.addBuildChecks
         (liqwid-nix.enableFormatCheck [
           "-XTemplateHaskell"
           "-XOverloadedRecordDot"
           "-XTypeApplications"
           "-XPatternSynonyms"
+          "-XNoFieldSelectors"
+          "-XImportQualifiedPost"
         ])
         liqwid-nix.enableCabalFormatCheck
         liqwid-nix.enableNixFormatCheck
